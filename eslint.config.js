44031dfbb489f917cbdd860b2d43ef2e7c3e3e6b
Sewook Kim <ninthsun91@meshed3d.com>
2025-06-02 22:59:16 +0900

    chore: remove unnecessary deps for monorepo

--- eslint.config.js
+++ eslint.config.js
@@ -1,41 +1,9 @@
-import js from '@eslint/js';
-import stylistic from '@stylistic/eslint-plugin';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
-import pluginReact from 'eslint-plugin-react';
-import { defineConfig } from 'eslint/config';
+import remixEslintConfig from "@meshed/eslint-config/remix";
+import { defineConfig } from "eslint/config";
 
 export default defineConfig([
+  ...remixEslintConfig,
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    plugins: { js },
-    extends: ['js/recommended'],
+    // 필요에 따라 Custom룰 정의
   },
-  {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    languageOptions: { globals: { ...globals.browser, ...globals.node } },
-  },
-  tseslint.configs.recommended,
-  {
-    files: ['**/*.{jsx,tsx}'],
-    plugins: { react: pluginReact.configs.flat.recommended },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-    rules: {
-      'react/react-in-jsx-scope': 'off',
-      'react/prop-types': 'off',
-      'react/jsx-uses-react': 'off',
-      'react/jsx-uses-vars': 'off',
-      'react/display-name': 'off',
-      'react/require-default-props': 'off',
-      'no-empty-pattern': 'off',
-    },
-  },
-  stylistic.configs.customize({
-    semi: true,
-    braceStyle: '1tbs',
-  }),
 ]);
